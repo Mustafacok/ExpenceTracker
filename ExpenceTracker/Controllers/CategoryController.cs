b8ad6621a5155e7fe2ae6bfe2054566b85420b0c
Mustafa ÇOK <96659190+Mustafacok@users.noreply.github.com>
2023-04-10 22:38:54 +0300

    Asp.Net Core MVC Örnek Çalışma Devamı

--- ExpenceTracker/Controllers/CategoryController.cs
+++ ExpenceTracker/Controllers/CategoryController.cs
@@ -47,7 +47,7 @@ namespace ExpenceTracker.Controllers
         // GET: Category/Create
         public IActionResult Create()
         {
-            return View();
+            return View(new Category());
         }
 
         // POST: Category/Create
