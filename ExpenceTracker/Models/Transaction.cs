07fd987e6a379e3c5df9917a0a64a35a72df44ef
Mustafa ÇOK <96659190+Mustafacok@users.noreply.github.com>
2023-04-26 01:38:36 +0300

    Transaction Controller ve sayfaları eklendi, güncellemesi yapıldı.

--- ExpenceTracker/Models/Transaction.cs
+++ ExpenceTracker/Models/Transaction.cs
@@ -11,7 +11,7 @@ namespace ExpenceTracker.Models
 
         //CategoryId
         public int CategoryId { get; set; }
-        public Category Category { get; set; }
+        public Category? Category { get; set; }
         public int Amount { get; set; }
         [Column(TypeName = "nvarchar(75)")]
         public string? Note { get; set; }
